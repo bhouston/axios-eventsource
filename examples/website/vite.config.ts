d20998c8170753eb5d24127592d87e6d208c1d0a
Ben Houston <neuralsoft@gmail.com>
2026-05-28 09:12:47 -0400

    upgrade packages.

--- examples/website/vite.config.ts
+++ examples/website/vite.config.ts
@@ -2,8 +2,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [nitro(), viteTsconfigPaths(), tanstackStart(), react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [nitro(), tanstackStart(), react()],
 });
